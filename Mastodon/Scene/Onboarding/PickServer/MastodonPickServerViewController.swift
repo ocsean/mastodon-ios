ecc25fd9ec130155c9da8134e7e745c51e4a56b5
Nathan Mattes <hallo@bullenscheisse.de>
2022-12-15 08:02:48 +0100

    Show back-button on server-list (#690)

--- Mastodon/Scene/Onboarding/PickServer/MastodonPickServerViewController.swift
+++ Mastodon/Scene/Onboarding/PickServer/MastodonPickServerViewController.swift
@@ -65,8 +65,6 @@ extension MastodonPickServerViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.leftBarButtonItem = UIBarButtonItem()
-        
         setupOnboardingAppearance()
         defer { setupNavigationBarBackgroundView() }
 
